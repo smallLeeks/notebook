396e86bf7a3396d4ecabab4d4108ed59a1dc24e7
chenghui <1354332468@qq.com>
2021-03-02 09:58:15 +0800

    ‘提交

--- test.js
+++ test.js
@@ -334,3 +334,29 @@ function currying() {
   return _add;
 }
 currying(1)(2)(3)(4)(5);
+
+function foo() {
+  getName = function() {
+    console.log(1);
+  }
+  return this;
+}
+foo.getName = function() {
+  console.log(2);
+}
+foo.prototype.getName = function() {
+  console.log(3);
+}
+var getName = function() {
+  console.log(4);
+}
+function getName() {
+  console.log(5);
+}
+foo.getName();
+getName();
+foo().getName();
+getName();
+new foo.getName();
+new foo().getName();
+new new foo().getName();
